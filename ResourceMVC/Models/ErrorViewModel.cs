7e8ac1332eddfdef02b4827545062d534412977f
BaoHoang03111 <tan.thach36@gmail.com>
2026-01-02 13:56:26 +0700

    ,

--- ResourceMVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ResourceMVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
